799aa484b4114a7e0d4eeda1266efca0449a2532
Ad147 <37510522+Ad147@users.noreply.github.com>
2018-09-27 21:46:20 +0800

    LC707 modified

--- LC700+/LeetCode707_DesignLinkedList.cpp
+++ LC700+/LeetCode707_DesignLinkedList.cpp
@@ -13,6 +13,10 @@
 
 // solution ====================================================================
 
+// doubly linked list
+// 44 ms (22%)
+// -> 24 ms
+
 class MyLinkedList
 {
   public:
@@ -24,9 +28,9 @@ class MyLinkedList
     // get the index-th value
     int get(int index);
     // add a node at head
-    void addAtHead(int val);
+    void addAtHead(int val) { addAtIndex(0, val); }
     // add a node at tail
-    void addAtTail(int val);
+    void addAtTail(int val) { addAtIndex(siz, val); }
     // add a node before the index-th node
     void addAtIndex(int index, int val);
     // delete the index-th node
@@ -62,66 +66,44 @@ MyLinkedList::MyLinkedList()
 
 MyLinkedList::~MyLinkedList()
 {
-    for (int i = {0}; i != siz; ++i)
+    while (siz != 0)
         deleteAtIndex(0);
-
     delete head;
     delete tail;
 }
 
 int MyLinkedList::get(int index)
 {
-    if (index >= siz)
+    if (index < 0 || index >= siz)
         return -1;
 
-    auto p = head->next;
+    Node *p = head->next;
     for (int i = {0}; i != index; ++i)
         p = p->next;
 
     return p->value;
 }
 
-void MyLinkedList::addAtHead(int val)
-{
-    auto temp = head->next;
-    head->next = new Node(val, temp, head);
-    temp->prev = head->next;
-    ++siz;
-}
-
-void MyLinkedList::addAtTail(int val)
-{
-    auto temp = tail->prev;
-    tail->prev = new Node(val, tail, temp);
-    temp->next = tail->prev;
-    ++siz;
-}
-
 void MyLinkedList::addAtIndex(int index, int val)
 {
-    if (index > siz)
+    if (index < 0 || index > siz)
         return;
-    else if (index == siz)
-    {
-        addAtTail(val);
-        return;
-    }
 
-    auto p = head->next;
+    Node *p = head->next;
     for (int i = {0}; i != index; ++i)
         p = p->next;
 
-    auto temp = p->prev;
-    p->prev = new Node(val, p, temp);
-    temp->next = p->prev;
+    p->prev->next = new Node(val, p, p->prev);
+    p->prev = p->prev->next;
     ++siz;
 }
 
 void MyLinkedList::deleteAtIndex(int index)
 {
-    if (index >= siz)
+    if (index < 0 || index >= siz)
         return;
-    auto p = head->next;
+
+    Node *p = head->next;
     for (int i = {0}; i != index; ++i)
         p = p->next;
 
@@ -129,6 +111,121 @@ void MyLinkedList::deleteAtIndex(int index)
     p->next->prev = p->prev;
 
     delete p;
+    --siz;
+}
+
+// solution ====================================================================
+
+// singly linked list
+
+class MyLinkedList2
+{
+  public:
+    MyLinkedList2() : siz{0}, head{nullptr}, tail{nullptr} {};
+
+    int get(int index);
+    void addAtHead(int val);
+    void addAtTail(int val);
+    void addAtIndex(int index, int val);
+    void deleteAtIndex(int index);
+
+  private:
+    struct Node
+    {
+        Node(int v = 0, Node *n = nullptr)
+            : value{v}, next{n} {}
+
+        int value;
+        Node *next;
+    };
+
+    int siz;
+    Node *head;
+    Node *tail;
+};
+
+// member functions ------------------------------------------------------------
+
+int MyLinkedList2::get(int index)
+{
+    if (index >= siz || index < 0)
+        return -1;
+
+    auto p = head;
+    for (size_t i = {0}; i != index; ++i)
+        p = p->next;
+
+    return p->value;
+}
+
+void MyLinkedList2::addAtHead(int val)
+{
+    auto temp = head;
+    head = new Node(val, temp);
+    if (tail == nullptr)
+        tail = head;
+    ++siz;
+}
+
+void MyLinkedList2::addAtTail(int val)
+{
+    auto temp = tail;
+    tail = new Node(val, nullptr);
+    if (head == nullptr)
+        head = tail;
+    else
+        temp->next = tail;
+    ++siz;
+}
+
+void MyLinkedList2::addAtIndex(int index, int val)
+{
+    if (index > siz || index < 0)
+        return;
+    else if (index == 0)
+    {
+        addAtHead(val);
+        return;
+    }
+    else if (index == siz)
+    {
+        addAtTail(val);
+        return;
+    }
+
+    auto p = head;
+    for (size_t i = {0}; i != index - 1; ++i)
+        p = p->next;
+
+    auto temp = p;
+    p = new Node(val, temp->next);
+    temp->next = p;
+    ++siz;
+}
+
+void MyLinkedList2::deleteAtIndex(int index)
+{
+    if (index >= siz || index < 0)
+        return;
+    else if (index == 0)
+    {
+        auto temp = head;
+        head = head->next;
+        if (head == nullptr)
+            tail = nullptr;
+        delete temp;
+        --siz;
+        return;
+    }
+
+    auto p = head;
+    for (int i = {0}; i != index - 1; ++i)
+        p = p->next;
+
+    auto temp = p->next;
+    p->next = temp->next;
+
+    delete temp;
     --siz;
 }
 
--- README.md
+++ README.md
@@ -12,20 +12,19 @@ Ad
 Attempted
 --------------------------------------------------------------------------------
 
- - [LeetCode707_DesignLinkedList](\LC700+\LeetCode707_DesignLinkedList.cpp)
-
 Need to be improved
 --------------------------------------------------------------------------------
 
- - [LeetCode002_AddTwoNumbers](\LC000+\LeetCode002_AddTwoNumbers.cpp) (see official solution)
- - [LeetCode007_ReverseInteger](\LC000+\LeetCode007_ReverseInteger.cpp) (official solution not efficient)
- - [LeetCode009_PalindromeNumber](\LC000+\LeetCode009_PalindromeNumber.cpp) (official solution not efficient enough)
+ - [LeetCode002_AddTwoNumbers](\LC000+\LeetCode002_AddTwoNumbers.cpp) (to see official solution)
  - [LeetCode014_LongestCommonPrefix](\LC000+\LeetCode014_LongestCommonPrefix.cpp)
 
 Solved
 --------------------------------------------------------------------------------
 
  - [LeetCode001_TwoSum](\LC000+\LeetCode001_TwoSum.cpp)
+ - [LeetCode007_ReverseInteger](\LC000+\LeetCode007_ReverseInteger.cpp)
+ - [LeetCode009_PalindromeNumber](\LC000+\LeetCode009_PalindromeNumber.cpp)
+ - [LeetCode707_DesignLinkedList](\LC700+\LeetCode707_DesignLinkedList.cpp)
 
 --------------------------------------------------------------------------------
 
